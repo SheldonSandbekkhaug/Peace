48d5d51a751628a06e7de742b03a4056f2e52c96
Sheldon S <sheldon@sandbekkhaug.net>
2014-11-01 16:33:26 -0500

    Added more fields to PacketMessage Added more sample data for unit mappings Rearranged fields in Peace.java

--- core/src/com/SheldonSandbekkhaug/Peace/PacketMessage.java
+++ core/src/com/SheldonSandbekkhaug/Peace/PacketMessage.java
@@ -3,4 +3,10 @@ package com.SheldonSandbekkhaug.Peace;
 public class PacketMessage {
 	/* Anything in this class can be sent over the network */
 	public String message;
+	
+	public LocationID srcLocation;
+	public int srcTileNum;
+	
+	public LocationID targetLocation;
+	public int targetTileNum;
 }
