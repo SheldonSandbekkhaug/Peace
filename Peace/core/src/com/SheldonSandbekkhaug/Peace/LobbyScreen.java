856c93782480253d633b66aba243e855c01549f5
Sheldon S <sheldon@sandbekkhaug.net>
2014-11-30 17:04:26 -0600

    Added TextField and Button to menu using Scene2D Added the default LibGDX skin for menu widgets

--- Peace/core/src/com/SheldonSandbekkhaug/Peace/LobbyScreen.java
+++ Peace/core/src/com/SheldonSandbekkhaug/Peace/LobbyScreen.java
@@ -2,27 +2,74 @@ package com.SheldonSandbekkhaug.Peace;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.BitmapFont.TextBounds;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 public class LobbyScreen implements Screen {
-	final Peace game;
-	OrthographicCamera camera;
-	SpriteBatch batch;
-	BitmapFont font;
-	TextField serverIPField;
+	private final Peace game;
+	private OrthographicCamera camera;
+	private SpriteBatch batch; // TODO: do we need this here?
+	private TextField serverIPField;
+	private Stage stage;
+	private Skin skin;
+	private Table table;
 	
 	public LobbyScreen(final Peace gam) {
         game = gam;
         batch = new SpriteBatch();
-        font = new BitmapFont(); // Defaults to Arial
-        font.setColor(Color.BLACK);
-
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage); // Required for menu control
+        
+        // Create the LibGDX menu skin (not the same thing as a Peace skin)
+        TextureAtlas atlas = new TextureAtlas(
+        		Gdx.files.internal("menu_skin/uiskin.atlas"));
+        skin = new Skin(Gdx.files.internal("menu_skin/uiskin.json"));
+        skin.addRegions(atlas);
+        
+        // Create the main table
+        table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+        table.setSkin(skin);
+        
+        // Create welcome label
+        String welcomeMessage = "Welcome to Peace. Enter the server IP address.";
+        Label welcomeLabel = new Label(welcomeMessage, skin);
+        welcomeLabel.setX(MainGameScreen.WINDOW_WIDTH / 2 - welcomeLabel.getWidth() / 2);
+    	welcomeLabel.setY(MainGameScreen.WINDOW_WIDTH / 2 - welcomeLabel.getHeight() / 2);
+        table.addActor(welcomeLabel);
+        
+        // Create server IP address TextField
+        serverIPField = new TextField("127.168.0.1", skin);
+        serverIPField.setBounds(100, 200, 400, 100);
+        table.add(serverIPField);
+        
+        // Create the start Button
+        TextButton startGameButton = new TextButton("Start game", skin);
+        startGameButton.setX(400);
+        startGameButton.setY(400);
+        startGameButton.setWidth(120);
+        startGameButton.setHeight(80);
+        table.add(startGameButton);
+        
+        startGameButton.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				// Start Peace
+		        startGame();
+			}
+        });
+        
         camera = new OrthographicCamera();
         camera.setToOrtho(false, MainGameScreen.WINDOW_WIDTH, MainGameScreen.WINDOW_HEIGHT);
     }
@@ -36,18 +83,23 @@ public class LobbyScreen implements Screen {
         batch.setProjectionMatrix(camera.combined);
 
         batch.begin();
-        String label = "Welcome to Peace. Entere the server IP address.";
-        TextBounds bounds = font.getBounds(label);
-        float textX = MainGameScreen.WINDOW_WIDTH / 2 - bounds.width / 2;
-        float textY = MainGameScreen.WINDOW_HEIGHT / 2;
         
-        font.draw(batch, label, textX, textY);
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
+        
         batch.end();
-
-        if (Gdx.input.isTouched()) {
-            game.setScreen(new MainGameScreen(game));
-            dispose();
-        }
+	}
+	
+	/* Connect to the server to play the game. */
+	public void startGame()
+	{
+		String serverIP = serverIPField.getText();
+        System.out.println("server IP: " + serverIP);
+        
+        // TODO: use the server IP text to get an IP for Peace
+        
+		game.setScreen(new MainGameScreen(game));
+        dispose();
 	}
 
 	@Override
@@ -78,7 +130,8 @@ public class LobbyScreen implements Screen {
 	@Override
 	public void dispose() {
 		batch.dispose();
-		font.dispose();
+		stage.dispose();
+		skin.dispose();
 	}
 
 }
